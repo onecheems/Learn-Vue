a819134b65c813556c0c9eb3a67e1b5c64d7ec1b
onecheems <1514873543@qq.com>
2023-08-05 13:34:26 +0800

    feat:second commmit

---
+++ src/Time.vue
@@ -0,0 +1,34 @@
+<script>
+import { timecount } from '@/util/time.js'
+import { timer } from '@/util/time.js'
+export default {
+    name: 'Time',
+    setup() {
+        function controll() {
+            if (!timer.value) {
+                timer.value = setInterval(() => {
+                    timecount.value--
+                    if (timecount.value === 0) {
+                        clearInterval(timer.value)
+                        timer.value = null
+                    }
+                }, 1000)
+            }
+            else {
+                clearInterval(timer.value)
+                timer.value = null
+            }
+        }
+
+        return {
+            timecount,
+            controll,
+        }
+    }
+}
+</script>
+
+<template>
+    <div>{{ timecount }}</div>
+    <button v-on:click="controll">控制</button>
+</template>
---
+++ src/util/time.js
@@ -0,0 +1,6 @@
+import { ref } from 'vue'
+
+const timecount = ref(10)
+const timer =ref(null)
+
+export { timecount, timer}
